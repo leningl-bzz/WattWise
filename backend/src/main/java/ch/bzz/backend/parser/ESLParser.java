3a7cca1d0b17a052649ccf9e2248169330db321c
Lukas Lening <109432969+leningl-bzz@users.noreply.github.com>
2025-05-19 09:09:44 +0200

    Parser impl

---
+++ backend/src/main/java/ch/bzz/backend/parser/ESLParser.java
@@ -0,0 +1,46 @@
+package ch.bzz.backend.parser;
+
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ESLParser {
+
+    public static Map<String, Double> parseESLFile(File xmlFile) {
+        Map<String, Double> obisValues = new HashMap<>();
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList valueRows = doc.getElementsByTagName("ValueRow");
+
+            for (int i = 0; i < valueRows.getLength(); i++) {
+                Node node = valueRows.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String obis = element.getAttribute("obis");
+                    String valueStr = element.getAttribute("value");
+                    if (!obis.isEmpty() && !valueStr.isEmpty()) {
+                        try {
+                            double value = Double.parseDouble(valueStr);
+                            obisValues.put(obis, value);
+                        } catch (NumberFormatException e) {
+                            System.err.println("Fehlerhafte Zahl: " + valueStr);
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return obisValues;
+    }
+}
